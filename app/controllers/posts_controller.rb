2e908b57e9d5e357d4c6d71e1bd0cb61fde63f84
kevinrogerteng <kevin.roger.teng@gmail.com>
2014-03-23 10:00:59 -0700

    adding angular

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,10 +1,11 @@
 class PostsController < ApplicationController
+  respond_to :json
   def index
-    posts = Post.includes(:tickets)
-    respond_to do |f|
-        f.html {render :layout => false }
-        f.json {render :json => {"posts" => posts.as_json(include: :tickets)}}
-    end
+    # posts = Post.includes(:tickets)
+    # respond_to do |f|
+    #     f.html
+    #     f.json {render :json => {"posts" => posts.as_json(include: :tickets)}}
+    # end
   end
 
   def create
